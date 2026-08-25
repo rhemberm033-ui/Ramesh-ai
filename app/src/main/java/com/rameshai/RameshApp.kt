de8412af98947963a74a35e9d702dd63070c6383
rhemberm033-ui <rhemberm033@gmail.com>
2026-08-25 12:30:49 +0530

    Update RameshApp.kt

--- app/src/main/java/com/rameshai/RameshApp.kt
+++ app/src/main/java/com/rameshai/RameshApp.kt
@@ -1,38 +1,10 @@
+
 package com.rameshai
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.rameshai.ui.chat.ChatScreen
-import com.rameshai.ui.theme.RameshAITheme
+import android.app.Application
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RameshAITheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = "chat"
-                    ) {
-                        composable("chat") {
-                            ChatScreen(navController = navController)
-                        }
-                    }
-                }
-            }
-        }
+class RameshApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
     }
 }
